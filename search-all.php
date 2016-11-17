c1ae1446136cbb844c5c88783c2734e7a184a19f
mpallansch <mkp95@uga.edu>
2016-11-16 21:32:05 -0500

    Edits Output Format

--- search-all.php
+++ search-all.php
@@ -24,19 +24,23 @@
             $actorid =  get_actor_id($firstname, $lastname);
             
             if($actorid === -1){
-                echo "no actor found by that name";
+                echo "Actor " . $firstname . " " . $lastname . " not found";
                 exit;
             }
             
-            $query = "SELECT * FROM roles, movies WHERE roles.movie_id = movies.id AND actor_id = " . $actorid;
+            $query = "SELECT * FROM roles, movies WHERE roles.movie_id = movies.id AND actor_id = " . $actorid . " ORDER BY movies.year DESC, movies.name ASC";
             $stmt = $conn->prepare($query);
 
             $stmt->execute();
 
             $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            echo '<table><tr><th>#</th><th>Title</th><th>Year</th></tr>';
+            $index = 1;
             while($row = $stmt->fetch()){
-                echo $row["name"] . '<br>';
+                echo '<tr><td>' . $index . '</td><td>' .  $row["name"] . '</td><td>' . $row['year'] . '</td></tr>';
+                $index++;
             }
+            echo '</table>';
             
         } catch (PDOException $e) {
             die('Database connection failed: ' . $e->getMessage());
--- search-kevin.php
+++ search-kevin.php
@@ -24,20 +24,24 @@
             $actorid =  get_actor_id($firstname, $lastname);
             
             if($actorid === -1){
-                echo "no actor found by that name";
+                echo  "Actor " . $firstname . " " . $lastname . " not found";
                 exit;
             }
             
             
-            $query = "SELECT * FROM movies m JOIN roles r ON r.movie_id = m.id JOIN actors a ON r.actor_id = a.id JOIN roles rr ON rr.movie_id = m.id JOIN actors aa ON rr.actor_id = aa.id WHERE r.movie_id = rr.movie_id AND r.actor_id = '" . $actorid . "' AND rr.actor_id = " . $kevinsid;
+            $query = "SELECT * FROM movies m JOIN roles r ON r.movie_id = m.id JOIN actors a ON r.actor_id = a.id JOIN roles rr ON rr.movie_id = m.id JOIN actors aa ON rr.actor_id = aa.id WHERE r.movie_id = rr.movie_id AND r.actor_id = '" . $actorid . "' AND rr.actor_id = " . $kevinsid . " ORDER BY m.year DESC, m.name ASC";
             $stmt = $conn->prepare($query);
 
             $stmt->execute();
 
             $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            echo '<table><tr><th>#</th><th>Title</th><th>Year</th></tr>';
+            $index = 1;
             while($row = $stmt->fetch()){
-                echo $row["name"] . '<br>';
+                echo '<tr><td>' . $index . '</td><td>' .  $row["name"] . '</td><td>' . $row['year'] . '</td></tr>';
+                $index++;
             }
+            echo '</table>';
             
         } catch (PDOException $e) {
             die('Database connection failed: ' . $e->getMessage());
